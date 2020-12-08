c18cac8604ede593c1dc366bbf431948c925d0df
Fahran Deen <fahrandeen17@gmail.com>
2020-12-08 20:19:05 +0530

    cross origin -  heroku server

--- src/main/java/com/springboot/crud/springbootfullstack/course/CourseResource.java
+++ src/main/java/com/springboot/crud/springbootfullstack/course/CourseResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200"})
+@CrossOrigin(origins = { "http://localhost:3000", "https://my-react-app20201208.herokuapp.com"})
 @RestController
 public class CourseResource {
 
